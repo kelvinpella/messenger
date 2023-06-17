d207e148e22bf6beb1cc5569de301e4ebda004bc
Kelvin <kelvinpella@gmail.com>
2023-06-17 13:15:44 +0300

    fetched current user

--- components/Sidebar/DesktopSidebar.tsx
+++ components/Sidebar/DesktopSidebar.tsx
@@ -2,10 +2,15 @@
 import { useState } from "react";
 import useRoutes from "@/common/hooks/useRoutes";
 import DesktopItem from "./DesktopItem";
+import { User } from "@prisma/client";
 
-export default function DesktopSidebar() {
+interface DesktopSidebarProps {
+  currentUser: User | null;
+}
+export default function DesktopSidebar({ currentUser }: DesktopSidebarProps) {
   const routes = useRoutes();
   const [isOpen, setIsOpoen] = useState(false);
+  console.log(currentUser);
   return (
     <div className="hidden lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:w-20 xl:px-6 lg:overflow-y-auto lg:bg-white lg:border-r-[1px] lg:pb-4 lg:flex lg:flex-col justify-between">
       <nav className="mt-4 flex flex-col justify-between">
